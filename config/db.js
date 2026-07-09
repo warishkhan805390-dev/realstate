ddc98161a0f6572e0ff8446241a8dede598f7f2a
warishkhan805390-dev <warishkhan805390@gmail.com>
2026-07-09 22:45:34 +0530

    fix: changed db connection to ES modules

--- config/db.js
+++ config/db.js
@@ -1,4 +1,25 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGODB_URI);
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(`MongoDB connection error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+
+
+
+
+
+
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
@@ -10,4 +31,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
